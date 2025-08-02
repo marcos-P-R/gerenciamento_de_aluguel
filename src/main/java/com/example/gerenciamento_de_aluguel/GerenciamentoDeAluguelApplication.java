26080991fc7c8f7a4cd1113f627b4d6a9bbfa586
makoto <marcos.p.mcruz@gmail.com>
2025-08-02 15:42:13 -0300

    Initialize Spring Boot project with Maven wrapper and basic application structure

---
+++ src/main/java/com/example/gerenciamento_de_aluguel/GerenciamentoDeAluguelApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_de_aluguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeAluguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeAluguelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gerenciamento_de_aluguel/GerenciamentoDeAluguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_de_aluguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeAluguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
