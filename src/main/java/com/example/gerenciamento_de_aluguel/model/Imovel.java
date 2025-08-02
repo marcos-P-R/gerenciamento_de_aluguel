f5043db82bd3c76a82f194476e2eb33233c9ec6f
makoto <marcos.p.mcruz@gmail.com>
2025-08-02 17:37:58 -0300

    Add Imovel model entity class

---
+++ src/main/java/com/example/gerenciamento_de_aluguel/model/Imovel.java
@@ -0,0 +1,83 @@
+package com.example.gerenciamento_de_aluguel.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Imovel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "tipo")
+    private String tipo;
+
+    @Column(name = "endereco_id")
+    private Integer enderecoId;
+
+    @Column(name = "capacidade")
+    private Integer capacidade;
+
+    @Column(name = "num_quartos")
+    private Integer numeroQuartos;
+
+    @Column(name = "disponibilidade")
+    private Boolean disponivel;
+
+    public Imovel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getEnderecoId() {
+        return enderecoId;
+    }
+
+    public void setEnderecoId(Integer enderecoId) {
+        this.enderecoId = enderecoId;
+    }
+
+    public Integer getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(Integer capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public Integer getNumeroQuartos() {
+        return numeroQuartos;
+    }
+
+    public void setNumeroQuartos(Integer numeroQuartos) {
+        this.numeroQuartos = numeroQuartos;
+    }
+
+    public Boolean getDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(Boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    
+}
